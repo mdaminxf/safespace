a9dfe3fc607d9d1ebe892397acaee6d316343f37
mdaminxf <mdmushrraf1234@gmail.com>
2025-09-03 23:42:18 +0530

    u

--- app/fraud-check/docs/page.tsx
+++ app/fraud-check/docs/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
-import * as pdfjsLib from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+import * as pdfjsLib from 'pdfjs-dist/webpack';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
